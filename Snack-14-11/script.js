9892339b8c46fe34bcdc957dc53c820bc36bbd78
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-14 12:21:35 +0100

    ciclo for non funzionante: non prende la i

--- Snack-14-11/script.js
+++ Snack-14-11/script.js
@@ -4,8 +4,8 @@
 // creo un elemento che devo ripetere
 const ul = document.querySelector('ul.lista');
 
-for (let i = 0, i >= 6, i++) {
-    const elemento = <li class="box box--"></li>;
+for (let i = 0, index >= 6, index++) {
+    const elemento = <li class="box box--$(index)">${index}</li>;
     ul.innerHTML += elemento;
 
 }
