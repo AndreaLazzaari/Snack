24c3cfcfcab361cc1e53b35e595d77ed5c2b24ae
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-14 12:35:49 +0100

    i ancora non funzionante

--- Snack-14-11/script.js
+++ Snack-14-11/script.js
@@ -2,10 +2,10 @@
 // Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
 
 // creo un elemento che devo ripetere
-const ul = document.querySelector('ul.lista');
+const ulElement = document.querySelector('ul.lista');
 
-for (let i = 0, index >= 6, index++) {
-    const elemento = <li class="box box--$(index)">${index}</li>;
-    ul.innerHTML += elemento;
+for (let i = 0, i <= 6, i++) {
+    
+   ulElement.innerHTML += '<p> ' + i + '</p>';
 
 }
