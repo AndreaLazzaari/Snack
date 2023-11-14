a164971014f6c5a4b34be56798aad25132a7ad37
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-14 12:17:35 +0100

    inserito ciclo for

--- Snack-14-11/script.js
+++ Snack-14-11/script.js
@@ -2,8 +2,10 @@
 // Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
 
 // creo un elemento che devo ripetere
+const ul = document.querySelector('ul.lista');
 
-for (let i = 0, i >=6, i++) {
-    
+for (let i = 0, i >= 6, i++) {
+    const elemento = <li class="box box--"></li>;
+    ul.innerHTML += elemento;
 
 }
