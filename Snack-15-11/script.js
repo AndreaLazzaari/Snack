9c7144714719c9bfd1961a35535ac102920fa68f
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-15 12:11:52 +0100

    corretto errore ortografico variabile

--- Snack-15-11/script.js
+++ Snack-15-11/script.js
@@ -1,6 +1,6 @@
 // Ciclare per i primi 100 numeri e inserire in un array esclusivamente i numeri divisibili per 7 o per 8.
 
 for (let i = 0; i <= 100; i++){
-    newArraay = [(i % 7 === 0), (i % 8 === 0)]
+    newArray = [(i % 7 === 0), (i % 8 === 0)]
     console.log(newArray)
 }
