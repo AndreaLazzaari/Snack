9952bbb01a9c20be6585d4daacc3e995e2ec5187
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-15 12:23:58 +0100

    esercizio corretto grazie livecoding

--- Snack-15-11/script.js
+++ Snack-15-11/script.js
@@ -1,6 +1,11 @@
 // Ciclare per i primi 100 numeri e inserire in un array esclusivamente i numeri divisibili per 7 o per 8.
 
-for (let i = 0; i <= 100; i++){
-    newArray = [(i % 7 === 0), (i % 8 === 0)]
-    console.log(newArray)
+const newArray = [];
+
+for (let i = 1; i <= 100; i++){
+    if (i % 7 ===0 || i % 8 === 0){
+    newArray.push(i)
+    }
 }
+
+console.log(newArray)
